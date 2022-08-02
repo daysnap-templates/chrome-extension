174b43e682f0f72cfd3831473433607bfe462ba4
chenzhigang <979703986@qq.com>
2022-08-02 14:49:32 +0800

    feat: husky

--- src/entry/popup/index.tsx
+++ src/entry/popup/index.tsx
@@ -5,8 +5,5 @@ const App = () => {
   return <div>1111</div>
 }
 
-const d = 1
-const a = 1
-
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(<App />)
